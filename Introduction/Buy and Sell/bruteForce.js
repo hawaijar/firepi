47418c00847a48c73d31acc4895a022612cdc962
hawaijar <hawaijar.js@gmail.com>
2021-07-29 21:43:24 +0530

    Added Buy/Sell - Brute force

---
+++ Introduction/Buy and Sell/bruteForce.js
@@ -0,0 +1,24 @@
+/*
+Best Time to Buy and Sell Stock
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+*/
+
+function maxProfit(prices) {
+  let max = 0;
+  for (let i = 0; i < prices.length; i++) {
+    for (let j = i + 1; j < prices.length; j++) {
+      const previous = prices[i];
+      const current = prices[j];
+      const profit = current - previous;
+      if (profit > max) {
+        max = profit;
+      }
+    }
+  }
+  return max;
+}
+
+const prices = [7, 1, 5, 3, 6, 4];
+console.log(maxProfit(prices));
