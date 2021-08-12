184ca136bd30e8bafba46609fc41fa4487c89339
hawaijar <hawaijar.js@gmail.com>
2021-08-12 10:15:55 +0530

    NNumberSum

--- Arrays/AlgoExpert/fourSum.js
+++ Arrays/AlgoExpert/fourSum.js
@@ -1,9 +1,9 @@
-function fourNumberSum(a, targetSum) {
+function NNumberSum(a, targetSum, size) {
   const result = [];
   const sum = (arr) => arr.reduce((a, b) => a + b, 0);
 
   function backtrack(prefix, array, target) {
-    if (prefix.length === 4 && sum(prefix) === targetSum) {
+    if (prefix.length === size && sum(prefix) === targetSum) {
       result.push(prefix.slice());
     } else if (prefix.length < 4) {
       for (let i = 0; i < array.length; i++) {
@@ -20,7 +20,8 @@ function fourNumberSum(a, targetSum) {
 
 // const a = [7, 6, 4, -1, 1, 2];
 // const t = 16;
-const a = [1, 0, -1, 0, -2, 2];
-const t = 0;
-
-console.log(fourNumberSum(a, t));
+// const a = [1, 0, -1, 0, -2, 2];
+// const t = 0;
+const a = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const t = 17;
+console.log(NNumberSum(a, t, 2));
